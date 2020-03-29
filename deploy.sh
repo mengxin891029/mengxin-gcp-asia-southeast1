40891dc5b0c7a2499d8cb15501d96820c4955fc2
Xin Meng <xin@cloudflare.com>
2020-03-29 15:44:12 +0800

    add travis CI initial integration

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+# build docker images
+docker build -t mengxin891029/sea-mengxin-ml:latest -t mengxin891029/sea-mengxin-ml:$GIT_SHA -f ./docker-images/sea/Dockerfile ./docker-images/sea
+docker push mengxin891029/sea-mengxin-ml:$GIT_SHA
+docker push mengxin891029/sea-mengxin-ml:latest
+
+
+# update kubernetes
+# kubectl apply -f k8s
+# kubectl set image deployments/sea-mengxin-ml-deployment sea-mengxin-ml=mengxin891029/sea-mengxin-ml:$GIT_SHA
