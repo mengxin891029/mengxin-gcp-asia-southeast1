c37302c39f57f6334bcdaac2228681cedbeded73
Xin Meng <xin@cloudflare.com>
2020-04-07 17:18:25 +0800

    Updated TCP namespace

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,10 @@ docker push mengxin891029/tunnel-mengxin-ml:$GIT_SHA
 docker push mengxin891029/tunnel-mengxin-ml:latest
 
 
-# update kubernetes
+# update kubernetes namespace: web
 kubectl apply -f k8s/web
 kubectl set image deployments/sea-mengxin-ml-deployment sea-mengxin-ml=mengxin891029/sea-mengxin-ml:$GIT_SHA
 kubectl set image deployments/tunnel-mengxin-ml-deployment tunnel-mengxin-ml=mengxin891029/tunnel-mengxin-ml:$GIT_SHA
+
+# update kubernetes namespace: tcp
+kubectl apply -f k8s/tcp
