5ded0992194311f4a17e60a01b9ca2d3117fba64
Xin Meng <xin@cloudflare.com>
2020-04-08 21:20:40 +0800

    reorganize the k8s folder structure optimized the keyless image

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,64 @@
-# build docker images
-docker build -t mengxin891029/sea-mengxin-ml:latest -t mengxin891029/sea-mengxin-ml:$GIT_SHA -f ./docker-images/sea/Dockerfile ./docker-images/sea
-docker build -t mengxin891029/tunnel-mengxin-ml:latest -t mengxin891029/tunnel-mengxin-ml:$GIT_SHA -f ./docker-images/tunnel/Dockerfile ./docker-images/tunnel
-docker build -t mengxin891029/keyless:latest -t mengxin891029/keyless:$GIT_SHA -f ./docker-images/keyless/Dockerfile ./docker-images/keyless
+###################################################################
+#                                                                 #
+#                    Start of Kubenetes Update                    #
+#                                                                 #
+#                         Namespace: web                          #
+#                                                                 #
+###################################################################
 
-# push dokcer images
+
+# update sea-mengxin-ml
+docker build -t mengxin891029/sea-mengxin-ml:latest -t mengxin891029/sea-mengxin-ml:$GIT_SHA -f ./docker-images/sea/Dockerfile ./docker-images/sea
 docker push mengxin891029/sea-mengxin-ml:$GIT_SHA
 docker push mengxin891029/sea-mengxin-ml:latest
+kubectl apply -f k8s/web/sea-mengxin-ml
+kubectl set image --namespace web deployments/sea-mengxin-ml-deployment sea-mengxin-ml=mengxin891029/sea-mengxin-ml:$GIT_SHA
+
+
+# update httpbin-mengxin-ml
+kubectl apply -f k8s/web/httpbin-mengxin-ml
+
+
+# update tunnel-mengxin-ml
+docker build -t mengxin891029/tunnel-mengxin-ml:latest -t mengxin891029/tunnel-mengxin-ml:$GIT_SHA -f ./docker-images/tunnel/Dockerfile ./docker-images/tunnel
 docker push mengxin891029/tunnel-mengxin-ml:$GIT_SHA
 docker push mengxin891029/tunnel-mengxin-ml:latest
-docker push mengxin891029/keyless:$GIT_SHA
-docker push mengxin891029/keyless:latest
-
-
-# update kubernetes namespace: web
-kubectl apply -f k8s/web
-
-kubectl set image --namespace web deployments/sea-mengxin-ml-deployment sea-mengxin-ml=mengxin891029/sea-mengxin-ml:$GIT_SHA
+kubectl apply -f k8s/web/tunnel-mengxin-ml
 kubectl set image --namespace web deployments/tunnel-mengxin-ml-deployment tunnel-mengxin-ml=mengxin891029/tunnel-mengxin-ml:$GIT_SHA
 
 
-# update kubernetes namespace: tcp
-kubectl apply -f k8s/tcp
+
+# update general kubernetes namespace: web
+kubectl apply -f k8s/web
+
+
+
+
+
+###################################################################
+#                                                                 #
+#                    Start of Kubenetes Update                    #
+#                                                                 #
+#                         Namespace: tcp                          #
+#                                                                 #
+###################################################################
+
+
+# update ssl-mengxin-ml-keyless
+docker build -t mengxin891029/keyless:latest -t mengxin891029/keyless:$GIT_SHA -f ./docker-images/keyless/Dockerfile ./docker-images/keyless
+docker push mengxin891029/keyless:$GIT_SHA
+docker push mengxin891029/keyless:latest
+kubectl apply -f k8s/tcp/ssl-mengxin-ml-keyless
 kubectl set image --namespace tcp deployments/ssl-mengxin-ml-keyless-deployment ssl-mengxin-ml=mengxin891029/keyless:$GIT_SHA
+
+
+
+# update mengxin-sea1-test-vm
+kubectl apply -f k8s/tcp/mengxin-sea1-test-vm
+
+
+
+# update general kubernetes namespace: tcp
+kubectl apply -f k8s/tcp
+
+
